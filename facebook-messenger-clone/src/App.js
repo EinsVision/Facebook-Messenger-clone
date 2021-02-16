c7f3d1a49b11405386f3f303f4e51057dede3a42
EinsVision <einsvision@gmail.com>
2021-02-16 16:11:07 +0900

    useState 사용하기!

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -5,11 +5,11 @@ function App() {
   const [input, setInput] = useState(1);
   const increment = () => setInput(input + 1);
   const decrement = () => setInput(input - 1);
-  
+
   return (
     <div className="app">
       {/* Review React Hook */}
-      <h1>first variable {input}</h1>
+      <h1> useState : {input}</h1>
 
       <input />
       <button onClick={increment}>increment</button>
