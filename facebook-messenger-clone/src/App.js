69a20bddabdd4e94513c2aa171957008ed6dc900
EinsVision <einsvision@gmail.com>
2021-02-16 15:27:08 +0900

    first commit

---
+++ facebook-messenger-clone/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+  return (
+    <div className="app">
+      <h1>Facebook Messenger Clone Coding</h1>
+
+      <input />
+      <button>Send Message</button>
+      {/* input field */}
+      {/* button */}
+      {/* messeges themselves */}
+    </div>
+  );
+}
+
+export default App;
