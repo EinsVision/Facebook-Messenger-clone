7de6b1bee2b3a16c2cbd084e609e28514fdec958
EinsVision <einsvision@gmail.com>
2021-02-17 23:55:00 +0900

    user name & text matching

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -5,7 +5,10 @@ import Message from './Message';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([ 
+    {username:'sonny', text: 'hey guys'}, 
+    {username:'chagndae', text: 'whats up'} 
+  ]);
   const [username, setUsername] = useState('');
 
   // useState = variable in REACT
@@ -14,7 +17,8 @@ function App() {
   useEffect(() => {
     // run code here... 
     // if its blank inside [], this code runs ONCE when the app component loads 
-  }, []);
+    setUsername(prompt('please enter your name'));
+  }, [] );
 
   console.log(input);
   console.log(messages);
@@ -23,13 +27,13 @@ function App() {
   const sendMessage = (event) => {
     // all the logic to send a message goes
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text: input}]);
     setInput('');
   }
   return (
     <div className="app">
       <h1>Facebook messenger clone 🚀✨</h1>
-
+      <h2>Welcome {username}</h2>
       <form>
         <FormControl>
           <InputLabel>Enter a message...</InputLabel>
@@ -40,7 +44,7 @@ function App() {
             
       {
         messages.map(message => (
-          <Message text={message}/>
+          <Message username={message.username} text={message.text}/>
         ))
       }
     </div>
