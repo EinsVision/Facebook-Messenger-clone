befb06b594e3b318b9c0dc97e17a73e08e7a8464
EinsVision <einsvision@gmail.com>
2021-02-17 22:05:01 +0900

    Applied React hook

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState(1);
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // all the logic to send a message goes
+    setMessages([...messages, input]);
+    setInput('');
+  }
   return (
     <div className="app">
       <h1>Facebook messenger clone 🚀✨</h1>
       <input value={input} onChange={ event => setInput(event.target.value) }/>
-      <button>Send message</button>
+      <button onClick={sendMessage}>Send message</button>
 
       {/* input field */}
       {/* button */}
